c579f8744277e7d522045363b66e50f8dc787f24
jules <81225111+jules@users.noreply.github.com>
2023-07-13 22:26:06 +1000

    Create and read by ID hypermedia methods

--- src/hypermedia_handlers.rs
+++ src/hypermedia_handlers.rs
@@ -1,5 +1,10 @@
 use crate::{model, templater};
-use axum::{extract, http::StatusCode, response::IntoResponse, Extension, Json};
+use axum::{
+    extract,
+    http::StatusCode,
+    response::{IntoResponse, Redirect},
+    Extension, Json,
+};
 use sqlx::PgPool;
 
 // GET /
@@ -21,10 +26,10 @@ pub async fn get_all_themes(Extension(pool): Extension<PgPool>) -> impl IntoResp
 
     let themes = sqlx::query_as::<_, model::Theme>(&sql)
         .fetch_all(&pool)
-        .await
-        .unwrap();
+        .await;
 
-    (StatusCode::OK, Json(themes))
+    let template = templater::AllThemesTemplate::new(themes.ok().clone());
+    templater::HtmlTemplate(template).into_response()
 }
 
 // GET /objective/
@@ -88,16 +93,7 @@ pub async fn get_all_tasks(Extension(pool): Extension<PgPool>) -> impl IntoRespo
 }
 
 // GET /measure/
-pub async fn get_all_measures(Extension(pool): Extension<PgPool>) -> impl IntoResponse {
-    let sql = "SELECT * from measures".to_string();
-
-    let measurements = sqlx::query_as::<_, model::Measurement>(&sql)
-        .fetch_all(&pool)
-        .await
-        .unwrap();
-
-    (StatusCode::OK, Json(measurements))
-}
+pub async fn get_all_measures() {}
 
 // GET /theme/id
 pub async fn get_theme(
@@ -111,21 +107,20 @@ pub async fn get_theme(
     .fetch_one(&pool)
     .await;
 
-    let rows: Result<Vec<model::Objective>, _> = sqlx::query_as(
-        r#"SELECT *
-        FROM objectives
-        where objectives.theme_id = $1
-        ORDER BY objectives.objective_id;"#,
-    )
-    .bind(&theme_id)
-    .fetch_all(&pool)
-    .await;
-
     match theme_row {
         Ok(theme) => {
-            let objs: Option<Vec<model::Objective>> = rows.ok();
+            let objectives: Option<Vec<model::Objective>> = sqlx::query_as(
+                r#"SELECT *
+                FROM objectives
+                where objectives.theme_id = $1
+                ORDER BY objectives.objective_id;"#,
+            )
+            .bind(&theme_id)
+            .fetch_all(&pool)
+            .await
+            .ok();
             let template =
-                templater::ThemeTemplate::new(theme.title, theme.theme_status, objs.clone());
+                templater::ThemeTemplate::new(theme.title, theme.theme_status, objectives.clone());
             templater::HtmlTemplate(template).into_response()
         }
         Err(_) => {
@@ -141,7 +136,7 @@ pub async fn get_objective(
     Extension(pool): Extension<PgPool>,
     extract::Path(objective_id): extract::Path<i32>,
 ) -> axum::response::Response {
-    let obj_row = sqlx::query!(
+    let objective_row = sqlx::query!(
         r#"
         SELECT objectives.title, themes.theme_id, themes.title as theme_title 
         from objectives 
@@ -154,45 +149,48 @@ pub async fn get_objective(
     .fetch_one(&pool)
     .await;
 
-    let kr_rows: Result<Vec<model::KeyResult>, _> = sqlx::query_as(
-        r#"SELECT *
-        FROM keyresults
-        where keyresults.objective_id = $1
-        ORDER BY keyresults.keyresult_id;"#,
-    )
-    .bind(&objective_id)
-    .fetch_all(&pool)
-    .await;
+    match objective_row {
+        Ok(obj) => {
+            let kr_rows: Option<Vec<model::KeyResult>> = sqlx::query_as(
+                r#"SELECT *
+                FROM keyresults
+                where keyresults.objective_id = $1
+                ORDER BY keyresults.keyresult_id;"#,
+            )
+            .bind(&objective_id)
+            .fetch_all(&pool)
+            .await
+            .ok();
 
-    let ini_rows: Result<Vec<model::Initiative>, _> = sqlx::query_as(
-        r#"SELECT *
-        FROM initiatives
-        where initiatives.objective_id = $1
-        ORDER BY initiatives.initiative_id;"#,
-    )
-    .bind(&objective_id)
-    .fetch_all(&pool)
-    .await;
+            let ini_rows: Option<Vec<model::Initiative>> = sqlx::query_as(
+                r#"SELECT *
+                FROM initiatives
+                where initiatives.objective_id = $1
+                ORDER BY initiatives.initiative_id;"#,
+            )
+            .bind(&objective_id)
+            .fetch_all(&pool)
+            .await
+            .ok();
 
-    let proj_rows: Result<Vec<model::Project>, _> = sqlx::query_as(
-        r#"SELECT *
-        FROM projects
-        where projects.objective_id = $1
-        ORDER BY projects.project_id;"#,
-    )
-    .bind(&objective_id)
-    .fetch_all(&pool)
-    .await;
+            let proj_rows: Option<Vec<model::Project>> = sqlx::query_as(
+                r#"SELECT *
+                FROM projects
+                where projects.objective_id = $1
+                ORDER BY projects.project_id;"#,
+            )
+            .bind(&objective_id)
+            .fetch_all(&pool)
+            .await
+            .ok();
 
-    match obj_row {
-        Ok(row) => {
             let template = templater::ObjectiveTemplate::new(
-                row.title,
-                row.theme_id,
-                row.theme_title,
-                kr_rows.ok(),
-                ini_rows.ok(),
-                proj_rows.ok(),
+                obj.title,
+                obj.theme_id,
+                obj.theme_title,
+                kr_rows,
+                ini_rows,
+                proj_rows,
             );
             templater::HtmlTemplate(template).into_response()
         }
@@ -206,8 +204,128 @@ pub async fn get_objective(
     }
 }
 
+// GET /keyresult/id
+pub async fn get_keyresult(
+    Extension(pool): Extension<PgPool>,
+    extract::Path(keyresult_id): extract::Path<i32>,
+) -> axum::response::Response {
+    let keyresult_row = sqlx::query!(
+        r#"
+        SELECT keyresults.title, objectives.objective_id, objectives.title as objective_title 
+        FROM keyresults 
+        LEFT JOIN objectives 
+        ON keyresults.objective_id = objectives.objective_id 
+        WHERE keyresults.keyresult_id = $1;
+        "#,
+        keyresult_id
+    )
+    .fetch_one(&pool)
+    .await;
+
+    match keyresult_row {
+        Ok(keyresult) => {
+            let measurements: Option<Vec<model::Measurement>> = sqlx::query_as(
+                r#"SELECT *
+                FROM measurements
+                WHERE measurements.keyresult_id = $1
+                ORDER BY measurements.measurement_id;"#,
+            )
+            .bind(&keyresult_id)
+            .fetch_all(&pool)
+            .await
+            .ok();
+
+            let template = templater::KeyResultTemplate::new(
+                keyresult.title,
+                keyresult.objective_id,
+                keyresult.objective_title,
+                measurements,
+            );
+            templater::HtmlTemplate(template).into_response()
+        }
+        Err(_) => {
+            let template = templater::ErrorTemplate::new(
+                StatusCode::NOT_FOUND,
+                "Key Result Not Found".to_string(),
+            );
+            templater::HtmlTemplate(template).into_response()
+        }
+    }
+}
+
+// GET /initiative/id
+pub async fn get_initiative(
+    Extension(pool): Extension<PgPool>,
+    extract::Path(initiative_id): extract::Path<i32>,
+) -> axum::response::Response {
+    let initiative_row = sqlx::query!(
+        r#"
+        SELECT initiatives.title, objectives.objective_id, objectives.title as objective_title 
+        FROM initiatives 
+        LEFT JOIN objectives 
+        ON initiatives.objective_id = objectives.objective_id 
+        WHERE initiatives.initiative_id = $1;
+        "#,
+        initiative_id
+    )
+    .fetch_one(&pool)
+    .await;
+
+    match initiative_row {
+        Ok(initiative) => {
+            let template = templater::InitiativeTemplate::new(
+                initiative.title,
+                initiative.objective_id,
+                initiative.objective_title,
+            );
+            templater::HtmlTemplate(template).into_response()
+        }
+        Err(_) => {
+            let template = templater::ErrorTemplate::new(
+                StatusCode::NOT_FOUND,
+                "Key Result Not Found".to_string(),
+            );
+            templater::HtmlTemplate(template).into_response()
+        }
+    }
+}
+
 // GET /project/id
-pub async fn get_project() {}
+pub async fn get_project(
+    Extension(pool): Extension<PgPool>,
+    extract::Path(project_id): extract::Path<i32>,
+) -> axum::response::Response {
+    let project_row = sqlx::query!(
+        r#"
+        SELECT projects.title, objectives.objective_id, objectives.title as objective_title 
+        FROM projects 
+        LEFT JOIN objectives 
+        ON projects.objective_id = objectives.objective_id 
+        WHERE projects.project_id = $1;
+        "#,
+        project_id
+    )
+    .fetch_one(&pool)
+    .await;
+
+    match project_row {
+        Ok(project) => {
+            let template = templater::InitiativeTemplate::new(
+                project.title,
+                project.objective_id,
+                project.objective_title,
+            );
+            templater::HtmlTemplate(template).into_response()
+        }
+        Err(_) => {
+            let template = templater::ErrorTemplate::new(
+                StatusCode::NOT_FOUND,
+                "Key Result Not Found".to_string(),
+            );
+            templater::HtmlTemplate(template).into_response()
+        }
+    }
+}
 
 // GET /task/id
 pub async fn get_task() {}
@@ -216,25 +334,107 @@ pub async fn get_task() {}
 pub async fn get_measure() {}
 
 // POST /theme
-pub async fn add_theme() {}
+pub async fn add_theme(
+    Extension(pool): Extension<PgPool>,
+    extract::Json(create_theme): extract::Json<model::CreateTheme>,
+) -> Redirect {
+    let _ = sqlx::query(r#"INSERT INTO themes (title) VALUES ($1);"#)
+        .bind(create_theme.title)
+        .fetch_all(&pool)
+        .await;
+
+    Redirect::to("/theme")
+}
 
 // POST /objective
-pub async fn add_objective() {}
+pub async fn add_objective(
+    Extension(pool): Extension<PgPool>,
+    extract::Json(create_objective): extract::Json<model::CreateObjective>,
+) -> Redirect {
+    let _ = sqlx::query(r#"INSERT INTO objectives (title, theme_id) VALUES ($1, $2);"#)
+        .bind(create_objective.title)
+        .fetch_all(&pool)
+        .await;
+
+    Redirect::to("/objective")
+}
+
+// POST /keyresult
+pub async fn add_keyresult(
+    Extension(pool): Extension<PgPool>,
+    extract::Json(create_keyresult): extract::Json<model::CreateKeyResult>,
+) -> Redirect {
+    let _ = sqlx::query(r#"INSERT INTO keyresults (title, objective_id) VALUES ($1, $2);"#)
+        .bind(create_keyresult.title)
+        .fetch_all(&pool)
+        .await;
+
+    Redirect::to("/keyresult")
+}
+
+// POST /initiative
+pub async fn add_initiative(
+    Extension(pool): Extension<PgPool>,
+    extract::Json(create_initiative): extract::Json<model::CreateInitiative>,
+) -> Redirect {
+    let _ = sqlx::query(r#"INSERT INTO initiatives (title, objective_id) VALUES ($1, $2);"#)
+        .bind(create_initiative.title)
+        .fetch_all(&pool)
+        .await;
+
+    Redirect::to("/initiative")
+}
 
 // POST /project
-pub async fn add_project() {}
+pub async fn add_project(
+    Extension(pool): Extension<PgPool>,
+    extract::Json(create_project): extract::Json<model::CreateProject>,
+) -> Redirect {
+    let _ = sqlx::query(r#"INSERT INTO projects (title, objective_id) VALUES ($1, $2);"#)
+        .bind(create_project.title)
+        .fetch_all(&pool)
+        .await;
+
+    Redirect::to("/project")
+}
 
 // POST /task
-pub async fn add_task() {}
+pub async fn add_task(
+    Extension(pool): Extension<PgPool>,
+    extract::Json(create_task): extract::Json<model::CreateTask>,
+) -> Redirect {
+    let _ = sqlx::query(r#"INSERT INTO projects (title, project_id) VALUES ($1, $2);"#)
+        .bind(create_task.title)
+        .fetch_all(&pool)
+        .await;
+
+    Redirect::to("/task")
+}
 
 // POST /measure
-pub async fn add_measure() {}
+pub async fn add_measure(
+    Extension(pool): Extension<PgPool>,
+    extract::Json(create_measurement): extract::Json<model::CreateMeasurement>,
+) -> Redirect {
+    let _ = sqlx::query(r#"INSERT INTO measurements (title, keyresult_id) VALUES ($1, $2);"#)
+        .bind(create_measurement.title)
+        .fetch_all(&pool)
+        .await;
+
+    Redirect::to("/measurement")
+}
 
 // DELETE /theme/id
 pub async fn remove_theme() {}
 
 // DELETE /objective/id
 pub async fn remove_objective() {}
+
+// DELETE /keyresult/id
+pub async fn remove_keyresult() {}
+
+// DELETE /initiative/id
+pub async fn remove_initiative() {}
 
 // DELETE /project/id
 pub async fn remove_project() {}
@@ -250,6 +450,12 @@ pub async fn update_theme() {}
 
 // PUT /objective/id
 pub async fn update_objective() {}
+
+// PUT /keyresult/id
+pub async fn update_keyresult() {}
+
+// PUT /initiative/id
+pub async fn update_initiative() {}
 
 // PUT /project/id
 pub async fn update_project() {}
