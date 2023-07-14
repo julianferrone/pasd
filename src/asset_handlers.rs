63e145e9a1b5327fd33f3aa19c491e0b1ac3e7bb
jules <81225111+jules@users.noreply.github.com>
2023-07-14 18:11:33 +1000

    Static asset handlers

---
+++ src/asset_handlers.rs
@@ -0,0 +1,35 @@
+use axum::{
+    http::header::{self, HeaderMap, HeaderValue},
+    response::IntoResponse,
+};
+use axum_macros::debug_handler;
+use mime::{
+    // TEXT_CSS, 
+    TEXT_JAVASCRIPT,
+};
+
+const HTMX: &str = include_str!("../static/js/htmx.min.js");
+const HTMX_EXT_JSON: &str = include_str!("../static/js/json-enc.js");
+
+async fn asset(source: &'static [u8], ty: &'static str) -> impl IntoResponse {
+    let mut headermap = HeaderMap::new();
+    headermap.insert(header::CONTENT_TYPE, HeaderValue::from_static(ty));
+    (headermap, source)
+}
+
+// async fn css(source: &'static str) -> impl IntoResponse {
+//     asset(source.as_bytes(), TEXT_CSS.as_ref()).await
+// }
+
+async fn js(source: &'static str) -> impl IntoResponse {
+    asset(source.as_bytes(), TEXT_JAVASCRIPT.as_ref()).await
+}
+
+#[debug_handler]
+pub async fn htmx_js() -> impl IntoResponse {
+    js(HTMX).await
+}
+
+pub async fn htmx_ext_json_js() -> impl IntoResponse {
+    js(HTMX_EXT_JSON).await
+}
